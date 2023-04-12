a2a7e294a8fe8cb9a72f4f0b9b1e2d93b3e853a4
Aleksey Petrov (AlekPet) <alexepetrof@gmail.com>
2023-04-12 21:09:29 +0300

    Update install.py
    fix

--- install.py
+++ install.py
@@ -1,4 +1,13 @@
 import launch
+import os
 
-if not launch.is_installed('googletrans'):
-    launch.run_pip("install googletrans==3.1.0a0", "Need to prompt_translate")
+requirements = os.path.join(os.path.dirname(os.path.realpath(__file__)), "requirements.txt")
+
+with open(requirements) as file:
+    for l in file:
+        l = l.strip()
+        if launch.is_installed("googletrans==3.1.0a0"):
+            launch.run_pip("ininstall googletrans==3.1.0a0", f"Prompt_translate uninstall -> googletrans==3.1.0a0")
+        
+        if not launch.is_installed(l):
+            launch.run_pip(f"install {l}", f"Prompt_translate requirement -> {l}")
