274efdd3baad0a07d640741fa686e2e5b483a37d
Aleksey Petrov (AlekPet) <alexepetrof@gmail.com>
2023-04-12 21:10:01 +0300

    Update install.py
    fix

--- install.py
+++ install.py
@@ -7,7 +7,7 @@ with open(requirements) as file:
     for l in file:
         l = l.strip()
         if launch.is_installed("googletrans==3.1.0a0"):
-            launch.run_pip("ininstall googletrans==3.1.0a0", f"Prompt_translate uninstall -> googletrans==3.1.0a0")
+            launch.run_pip("uninstall googletrans==3.1.0a0", f"Prompt_translate uninstall -> googletrans==3.1.0a0")
         
         if not launch.is_installed(l):
             launch.run_pip(f"install {l}", f"Prompt_translate requirement -> {l}")
